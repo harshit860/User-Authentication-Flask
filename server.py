781c8c6e0350412e7f54d98c71d50246b98459d3
harshit860 <harshitga@gmail.com>
2019-11-24 14:35:03 +0530

    message route/ showmessage

--- server.py
+++ server.py
@@ -10,10 +10,16 @@ import uuid
 def read_user():                                                     # Reading file to load data for login
     global users
     users = list()
+    global message_list
+    message_list = list()
     with open("loginfile.csv") as login:
         loginfile = csv.DictReader(login)
         for i in loginfile:
             users.append(i)
+    with open("message.csv") as messages:
+        messages_user = csv.DictReader(messages) 
+        for j in messages_user:
+            message_list.append(j)
 
 
 
@@ -31,12 +37,12 @@ def write_file():
         writer = csv.DictWriter(writer,fieldnames=fieldnames)
         if not writer.fieldnames == fieldnames:
             writer.writeheader()
-        val = int(str(uuid.uuid4()))
+        val = str(uuid.uuid4())
         id = val
         username = request.json['username']
         password = request.json['password']
         writer.writerow({"id":id,"username":username,"password":password})
-    return json.dumps({"success":"success"+username})
+    return json.dumps({"success":"success"+username,"username":username})
 
 @app.route("/login",methods = ["POST"])                                            
 def login_route():
@@ -48,4 +54,41 @@ def login_route():
             return json.dumps({"success":i})
     
     return json.dumps({"success":"Wrong Username/Password"})
+
+
+@app.route("/sendmessage",methods = ["POST"])                                                   #Sening messages to user with message id
+def message():
+    read_user()
+    try:
+        with open("message.csv") as reader:
+            fieldnames = ['id','msg_id','msg','sender','receiver']
+    except FileNotFoundError:
+        with open("message.csv","w") as writer:
+            fieldnames = ['id','msg_id','msg','sender','receiver']
+            writer = csv.DictWriter(writer,fieldnames = fieldnames)
+            writer.writeheader()
+    with open("message.csv","a") as writer:
+        writer = csv.DictWriter(writer,fieldnames=fieldnames)
+        if not writer.fieldnames == fieldnames:
+            writer.writeheader()
+        val = str(uuid.uuid4())
+        print(val)
+        msg = request.json['msg']
+        id = int(request.json['id'])
+        sender = request.json['sender']
+        receiver = request.json['receiver']
+        writer.writerow({'id':id,'msg_id':val,'msg':msg,'sender':sender,'receiver':receiver})
+    return "msg sent"
+
+@app.route("/showmessage",methods =["POST"])                                                    #Showmessage
+def show_msg():
+    read_user()
+    messages = list()
+    username = request.json['username']
+    for i in message_list:
+        if i["receiver"] == username:
+            messages.append(i)
+    return json.dumps({"message_list":messages})
+
+
 
